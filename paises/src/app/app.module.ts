9ba1b74206a7ce097852b7b9054df628b4192422
Mtllns <miquelmtllns@pm.me>
2020-07-08 11:05:14 +0200

    Inicio proyecto  - Angular PWA

---
+++ paises/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { PaisComponent } from './pages/pais/pais.component';
+import { PaisesComponent } from './pages/paises/paises.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PaisComponent,
+    PaisesComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
